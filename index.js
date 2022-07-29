db645ab9a1f5cd0d57648fadfe86ca98d8fdcd94
Tyler French <tylerfrench89@gmail.com>
2022-07-28 19:06:00 -0700

    error message around passwords and inputs banner and overflow fix

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const errorMessage = document.getElementById("error_message")
-const password = document.querySelector(".password")
-const confirmedPassword = document.querySelector('.passwordConfirm')
+const inputs = document.querySelectorAll('input')
+const errorMessage = document.querySelector('.error_message')
+const pwd = document.querySelector('#password')
+const confirmPwd = document.querySelector('#passwordConfirm')
 const submitBtn = document.getElementById('signupBtn')
 
-console.log(submitBtn)
-
-submitBtn.addEventListener('click', (e) => {
-    console.log('test')
-    if(password.value !== confirmedPassword.value) {
-        console.log('NAH')
-        e.preventDefault()
+const checkInputs = () => {
+  console.log(confirmPwd.value, pwd.value)
+  console.log(errorMessage)
+  if (confirmPwd.value) {
+    if (pwd.value != confirmPwd.value) {
+      pwd.style.border = '1px solid red'
+      confirmPwd.style.border = '1px solid red'
+      errorMessage.style.display = 'block'
+      return false
+    } else {
+      pwd.style.border = '1px solid lightgrey'
+      confirmPwd.style.border = '1px solid lightgrey'
+      errorMessage.style.display = 'none'
     }
-})
+  }
+  inputs.forEach((input) => {
+    if (input.value.length <= 0) {
+      input.style.border = '1px solid red'
+    } else {
+      input.style.border = '1px solid lightgrey'
+    }
+  })
+  console.log('this works')
+}
